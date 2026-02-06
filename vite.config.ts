9c1fcc95b73a92a353c702a5b8be22a554441544
newstonnate-png <newstonnate@gmail.com>
2026-02-06 12:36:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded using relative paths
+  base: '/barakah-bridge/', 
 });
